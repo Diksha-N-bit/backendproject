0a58475655d05ec84019aa0547ec29274a70792e
Diksha <dikshasaini0320@gmail.com>
2026-03-27 12:05:36 +0530

    main utilites

--- OneDrive/Desktop/backendproj/src/index.js
+++ OneDrive/Desktop/backendproj/src/index.js
@@ -3,4 +3,12 @@ import connectDB from "../db/db.js";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("mongodb connection failed !!!", err);
+})
